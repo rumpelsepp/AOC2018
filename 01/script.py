1ef5ea19f9a288c9ed179d24ce40f33a1dece617
Stefan Tatschner <stefan@rumpelsepp.org>
2019-01-03 00:21:22 +0100

    Merge scripts

--- 01/script.py
+++ 01/script.py
@@ -2,11 +2,19 @@
 
 import sys
 
+# part 1
+data = [int(a) for a in sys.stdin.readlines()]
+sum_ = 0
+for line in data:
+    sum_ += line
+
+print(sum_)
+
+# part 2
 iterations = 0
 f = 0
 seen_frequencies = set()
 seen_frequencies.add(0)
-data = [int(a) for a in sys.stdin.readlines()]
 
 while True:
     for line in data:
