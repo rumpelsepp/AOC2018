67dff170d41036140feb04a106018f1ab1bd754f
Stefan Tatschner <stefan@rumpelsepp.org>
2019-01-03 00:16:46 +0100

    Merge scripts

--- 05/script.py
+++ 05/script.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 
 import sys
-import time
 
-data = sys.stdin.read().strip()
-deleted = [False for i in range(0, len(data))]
-run = True
 
-def data_generator():
+def data_generator(data, deleted):
     for i in range(0, len(data)):
         if deleted[i]:
             continue
@@ -15,26 +11,47 @@ def data_generator():
         yield i, data[i]
 
 
-while run:
-    run = False
-    prev = ''
-    prev_i = 0
+def run(data):
+    deleted = [False for i in range(0, len(data))]
+    run = True
+    while run:
+        run = False
+        prev = ''
+        prev_i = 0
 
-    for i, cur in data_generator():
-        if (prev.lower() == cur.lower()) and prev != cur:
-            deleted[i] = True
-            deleted[prev_i] = True
+        for i, cur in data_generator(data, deleted):
+            if (prev.lower() == cur.lower()) and prev != cur:
+                deleted[i] = True
+                deleted[prev_i] = True
 
-            run = True
-            prev = ''
-            continue
+                run = True
+                prev = ''
+                continue
 
-        prev = cur
-        prev_i = i
+            prev = cur
+            prev_i = i
 
-size = 0
-for _, cur in data_generator():
-    print(cur, end='')
-    size += 1
-print()
-print(size)
+    return len(list(data_generator(data, deleted)))
+
+
+def main():
+    data = sys.stdin.read().strip()
+
+    # part 1
+    print(run(data))
+
+    # part 2
+    best = len(data)
+    for i in range(ord('a'), ord('z')+1):
+        reduced_data = data.replace(chr(i), '').replace(chr(i).upper(), '')
+        length = run(reduced_data)
+        if length < best:
+            best = length
+
+        print(f'{chr(i)}: got length: {length}')
+
+    print(f'best is: {best}')
+
+
+if __name__ == '__main__':
+    main()
